6cf0e1b9674d27b62295b750fe8cdeedd679ba3a
Alexander Parvanov <47386361+YYx00xZZ@users.noreply.github.com>
2021-09-21 00:06:34 +0300

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,7 +2,7 @@ import pickle
 
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
+# import plotly.graph_objects as go
 import requests
 import streamlit as st
 from scipy.sparse import data
